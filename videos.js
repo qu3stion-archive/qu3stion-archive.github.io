acf8c728ec3fc52e90122f2ec61a0201deac3975
qu3stion-archive <308559968+qu3stion-archive@users.noreply.github.com>
2026-07-25 16:04:31 -0500

    quick save

--- videos.js
+++ videos.js
@@ -1,14 +1,49 @@
 const media = document.querySelector(".media");
+const eram = document.querySelector("#eram");
+// "eram" is my function testing dummy!!
 
-function build() {
-    for (x = 0; x < 20; x++) {
-        var article = document.createElement("article");
-        var img = document.createElement("img");
-        var p = document.createElement("p");
-        img.src = "media/qu3stion.jpg";
-        p.innerHTML = "Qu3stion";
-        article.appendChild(img);
-        article.appendChild(p);
-        media.appendChild(article);
+async function build() {
+    media.style.display = "none";
+    for (x = 1; x < 21; x++) {
+        try {
+            var article = document.createElement("article");
+            var img = document.createElement("img");
+            var p = document.createElement("p");
+
+            var REQUEST = await fetch("media/Q (" + x + ").jpg");
+            if (REQUEST.status == 404) {
+                console.log("eek! (404)")
+                continue
+            }
+            img.src = REQUEST.url;
+
+            p.innerHTML = "Qu3stion";
+            article.appendChild(img);
+            article.appendChild(p);
+            media.appendChild(article);
+        } catch (error) {
+            console.log("eek!")
+            return false;
+        }
     }
+    return true;
 };
+async function load() {
+    let check = await build();
+    if (check) {
+        console.log("archive loaded!")
+        media.style.display = "grid";
+    } else {
+        console.error("/!\ >> ARCHIVE FAILED TO LOAD")
+    }
+}
+async function cassarole() {
+    fetch("media/").then((response) => {
+        return response.text();
+    }).then((html) => {
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(html, "text/html");
+        console.log(doc);
+    })
+    //eram.src = REQUEST.url;
+}
