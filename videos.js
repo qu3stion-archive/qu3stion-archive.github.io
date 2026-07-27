966b9b8271d23cff7af53a4f131ddef0ed48a5d9
qu3stion-archive <308559968+qu3stion-archive@users.noreply.github.com>
2026-07-27 12:57:17 -0500

    progress on explorer

--- videos.js
+++ videos.js
@@ -1,9 +1,15 @@
-const media = document.querySelector(".explorer");
+const explorer = document.querySelector(".explorer");
 const spyglass = document.querySelector("#viewer");
+const message = document.querySelector("#status");
+//const eram = document.querySelector("#eram");
 
-const eram = document.querySelector("#eram");
+const accepted_file_types = [".jpg", ".mp4"]
 // "eram" is my function testing dummy!!
+const page_num = 50;
+let page = 1;
 
+let mediaURLs = [];
+let mediaIDs = [];
 
 let tagsAll = new Map();
 tagsAll.set("Q", {name: "Qu3stion",    class: "qu3stion",    color: "#FFF", description: ""});
@@ -25,53 +31,99 @@ tagsAll.set("f", {name: "Fish",         class: "fish",       color: "#FFF", desc
 tagsAll.set("a", {name: "Animal",       class: "animal",     color: "#FFF", description: ""});
 tagsAll.set("g", {name: "Games",        class: "Gaming",     color: "#FFF", description: ""});
 tagsAll.set("d", {name: "Drawings",     class: "drawings",   color: "#FFF", description: ""});
-async function build() {
-    media.style.display = "none";
-    for (x = 1; x < 21; x++) {
-        try {
-            var article = document.createElement("article");
-            var img = document.createElement("img");
-            var p = document.createElement("p");
-            var button = document.createElement("button");
 
-            var REQUEST = await fetch("media/Q (" + x + ").jpg");
-            if (REQUEST.status == 404) {
-                console.log("eek! (404)")
-                continue
+async function get() { // CODE IS JANKY, RENOVATE AFTER CSV IS DONE
+    for (x = 1; x < 855; x++) {
+        var REQUEST = await fetch("media/" + "m" + x + ".jpg")
+        if (REQUEST.status == 200) {
+            mediaURLs.push(REQUEST.url);
+        } else {
+            var REQUEST2 = await fetch("media/" + "m" + x + ".mp4")
+            if (REQUEST2.status == 200) {
+                mediaURLs.push(REQUEST2.url);
+            } else {
+                mediaURLs.push(null);
+                console.log(REQUEST2)
             }
-            var url = REQUEST.url
-            img.src = url;
-            //console.log(url)
-            p.innerHTML = "Qu3stion";
-            button.addEventListener("click", (event) => {
-                expand(event);
-            })
-
-            button.appendChild(img);
-            article.appendChild(button);
-            article.appendChild(p);
-            article.id = "Q (" + x + ")";
-            media.appendChild(article);
-        } catch (error) {
-            console.log("eek!")
-            return false;
         }
+        message.innerHTML = "fetching media... (" + x + "/" + 854 + ")"
+    }
+    return true;
+}
+async function build() {
+    explorer.style.display = "none";
+    for (x = 0; x < 855; x++) {
+        let mediaID = "m" + x;
+        var article = document.createElement("article");
+        var img = document.createElement("img");
+        var p = document.createElement("p");
+        var button = document.createElement("button");
+        var elm;
+        var url;
+        url = mediaURLs[x];
+        if (url === null) {
+            console.warn("m" + x + " is NULL.")
+            continue;
+        }
+        switch (url.slice(-3)) {
+            case "jpg":
+                elm = document.createElement("img");
+                elm.src = url;
+                break;
+            case "mp4":
+                elm = document.createElement("video");
+                elm.controls = false;
+                source = document.createElement("source");
+                source.src = url;
+                source.type = "video/mp4";
+                elm.appendChild(source);
+                break;
+        }
+        button.appendChild(elm);
+        console.log(url)
+        p.innerHTML = mediaID + url.slice(-4);
+        button.addEventListener("click", (event) => {
+            console.log("hmph!")
+            expand(event);
+        })
+        article.appendChild(button);
+        article.appendChild(p);
+        article.id = mediaID;
+        mediaIDs.push(mediaID);
+        article.classList.add("hidden")
+        explorer.appendChild(article);
     }
     return true;
 };
 async function load() {
-    let check = await build();
+    message.innerHTML = "fetching media..."
+    let check = await get();
     if (check) {
-        //console.log("archive loaded!")
-        media.style.display = "grid";
+        message.innerHTML = "building file explorer..."
+        let check2 = await build();
+        if (check2) {
+            explorer.style.display = "grid";
+            message.innerHTML = "archive loaded successfully! :3"
+        } else {
+            console.error("/!\ >> FAILED TO INIT EXPLORER");
+        }
     } else {
-        //console.error("/!\ >> ARCHIVE FAILED TO LOAD")
+        console.error("/!\ >> FAILED TO GET MEDIA");
     }
 }
 
 function expand(event) {
     const selected = event.target;
-    const url = selected.src;
+    let url
+    switch (selected.tagName) {
+        case "IMG":
+            url = selected.src;
+            break;
+        case "VIDEO":
+            url = selected.firstElementChild.src;
+            break;
+    }
+    console.log(url)
     if (spyglass.children.length > 0) {
         for (element of spyglass.children) {
             spyglass.removeChild(element);
@@ -87,6 +139,7 @@ function expand(event) {
         case "mp4":
             elm = document.createElement("video");
             elm.controls = true;
+            elm.autoplay = true;
             //elm.width = "100%"
             source = document.createElement("source");
             source.src = url;
