bcb59f52b4872b051da22492999fed13d71e7ab1
qu3stion-archive <308559968+qu3stion-archive@users.noreply.github.com>
2026-07-27 10:51:53 -0500

    file naming convention set

--- videos.js
+++ videos.js
@@ -1,28 +1,30 @@
-const media = document.querySelector(".media");
-const spyglass = document.querySelector("#spyglass");
+const media = document.querySelector(".explorer");
+const spyglass = document.querySelector("#viewer");
 
 const eram = document.querySelector("#eram");
 // "eram" is my function testing dummy!!
 
-const files = [ // Temporary way to pull files.
-
-]
-
 
 let tagsAll = new Map();
-tagsAll.set("Q", {name: "Qu3stion",    class: "qu3stion",    color: "#FFF"});
-tagsAll.set("X", {name: "Exclamation", class: "exclamation", color: "#FFF"});
-tagsAll.set("C", {name: "C0mma",       class: "c0mma",       color: "#FFF"});
-tagsAll.set("A", {name: "Ampersand",   class: "ampersand",   color: "#FFF"});
-tagsAll.set("E", {name: "Creator",     class: "creator",     color: "#FFF"});
-tagsAll.set("G", {name: "Green",       class: "green",       color: "#FFF"});
+tagsAll.set("Q", {name: "Qu3stion",    class: "qu3stion",    color: "#FFF", description: ""});
+tagsAll.set("X", {name: "3xclamation", class: "3xclamation", color: "#FFF", description: ""});
+tagsAll.set("C", {name: "C0mma",       class: "c0mma",       color: "#FFF", description: ""});
+tagsAll.set("A", {name: "Amp3rsand",   class: "amp3rsand",   color: "#FFF", description: ""});
+tagsAll.set("E", {name: "Creator",     class: "creator",     color: "#FFF", description: ""});
+tagsAll.set("G", {name: "Green",       class: "green",       color: "#FFF", description: ""});
+tagsAll.set("P", {name: "...Peri0d??", class: "peri0d",      color: "#FFF", description: ""});
 //
-tagsAll.set("s", {name: "Silly",       class: "silly",       color: "#FFF"});
-tagsAll.set("z", {name: "Spooky",      class: "spooky",      color: "#FFF"});
-tagsAll.set("e", {name: "Explosion",   class: "explosion",   color: "#FFF"});
-tagsAll.set("f", {name: "Fish",        class: "fish",        color: "#FFF"});
-tagsAll.set("a", {name: "Animal",      class: "animal",      color: "#FFF"});
-
+tagsAll.set("l", {name: "Lore",         class: "lore",       color: "#FFF", description: ""});
+tagsAll.set("i", {name: "Intro",        class: "intro",      color: "#FFF", description: "First time a character appears!"});
+tagsAll.set("s", {name: "Silly",        class: "silly",      color: "#FFF", description: ""});
+tagsAll.set("z", {name: "Spooky",       class: "spooky",     color: "#FFF", description: ""});
+tagsAll.set("m", {name: "Mean",         class: "mean",       color: "#FFF", description: "WHEN THEY'RE MEAN TO QU3STION    ON TWITTER:"});
+tagsAll.set("n", {name: "Nice",         class: "nice",       color: "#FFF", description: "WHEN THEY'RE [Nice! :3] TO QU3STION    ON TWITTER:"});
+tagsAll.set("e", {name: "Explosion",    class: "explosion", color: "#FFF", description: ""});
+tagsAll.set("f", {name: "Fish",         class: "fish",       color: "#FFF", description: ""});
+tagsAll.set("a", {name: "Animal",       class: "animal",     color: "#FFF", description: ""});
+tagsAll.set("g", {name: "Games",        class: "Gaming",     color: "#FFF", description: ""});
+tagsAll.set("d", {name: "Drawings",     class: "drawings",   color: "#FFF", description: ""});
 async function build() {
     media.style.display = "none";
     for (x = 1; x < 21; x++) {
