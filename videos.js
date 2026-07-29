8ac4bea8de7a55f28f0d1ad85eb69562228ae1eb
qu3stion-archive <308559968+qu3stion-archive@users.noreply.github.com>
2026-07-29 15:26:27 -0500

    tag reading complete

--- videos.js
+++ videos.js
@@ -12,6 +12,45 @@ var mediaCSVData = []; // Data parsed from the CSV.
 var mediaProcessedData = new Object(); // Contains all media objects.
 var mediaDisplayed = []; // IDs of all displayed medias.
 
+let tagsAll = new Map();
+tagsAll.set("?", {name: "Qu3stion",     class: "qu3stion",   color: "#FFF", description: ""});
+tagsAll.set("¿", {name: "Old Qu3stion", class: "qu3stion2",  color: "#FFF", description: ""});
+tagsAll.set("!", {name: "3xclamation",  class: "3xclamation",color: "#FFF", description: ""});
+tagsAll.set(",", {name: "C0mma",        class: "c0mma",      color: "#FFF", description: ""});
+tagsAll.set("&", {name: "Amp3rsand",    class: "amp3rsand",  color: "#FFF", description: ""});
+tagsAll.set(":", {name: "Creator",      class: "creator",    color: "#FFF", description: ""});
+tagsAll.set("G", {name: "Green",        class: "green",      color: "#FFF", description: ""});
+tagsAll.set(".", {name: "...Peri0d??",  class: "peri0d",     color: "#FFF", description: ""});
+//
+tagsAll.set("l", {name: "Lore",         class: "lore",       color: "#FFF", description: ""});
+tagsAll.set("i", {name: "Intro",        class: "intro",      color: "#FFF", description: "First time a character appears!"});
+tagsAll.set("s", {name: "Silly",        class: "silly",      color: "#FFF", description: ""});
+tagsAll.set("z", {name: "Spooky",       class: "spooky",     color: "#FFF", description: ""});
+tagsAll.set("m", {name: "Mean",         class: "mean",       color: "#FFF", description: "WHEN THEY'RE MEAN TO QU3STION    ON TWITTER:"});
+tagsAll.set("n", {name: "Nice",         class: "nice",       color: "#FFF", description: "WHEN THEY'RE [Nice! :3] TO QU3STION    ON TWITTER:"});
+tagsAll.set("x", {name: "Explosion",    class: "explosion",  color: "#FFF", description: ""});
+tagsAll.set("f", {name: "Fish",         class: "fish",       color: "#FFF", description: ""});
+tagsAll.set("a", {name: "Animal",       class: "animal",     color: "#FFF", description: ""});
+tagsAll.set("g", {name: "Games",        class: "gaming",     color: "#FFF", description: ""});
+tagsAll.set("d", {name: "Drawings",     class: "drawings",   color: "#FFF", description: ""});
+tagsAll.set("c", {name: "X-mas",        class: "x-mas",      color: "#FFF", description: ""});
+tagsAll.set("h", {name: "Halloween",    class: "halloween",  color: "#FFF", description: ""});
+
+let tagsDate = new Map();
+tagsDate.set(0, {name: "August '25",     class: "august25"});
+tagsDate.set(1, {name: "September '25",  class: "september25"});
+tagsDate.set(2, {name: "October '25",    class: "october25"});
+tagsDate.set(3, {name: "November '25",   class: "november25"});
+tagsDate.set(4, {name: "December '25",   class: "december25"});
+tagsDate.set(5, {name: "January '26",    class: "january26"});
+tagsDate.set(6, {name: "February '26",   class: "february26"});
+tagsDate.set(7, {name: "March '26",      class: "march26"});
+tagsDate.set(9, {name: "April '26",      class: "april26"});
+tagsDate.set(10,{name: "May '26",        class: "may26"});
+tagsDate.set(11,{name: "June '26",       class: "june26"});
+tagsDate.set(12,{name: "July '26",       class: "july26"});
+tagsDate.set(13,{name: "August '26",     class: "august26"});
+
 const mediaTemplate = {
     id      : undefined,
     filetype: undefined,
@@ -38,6 +77,14 @@ const mediaTemplate = {
         clone.querySelectorAll("button")[0].addEventListener("click", (event) => {
             expand(event)
         });
+        for (tag of this.tags) {
+            var box = clone.querySelectorAll(".tags")[0];
+            var card = document.createElement("label");
+            card.innerHTML = tag["name"];
+            clone.querySelector("article").classList.add(tags["class"])
+            box.appendChild(card);
+        }
+        clone.querySelectorAll(".div_id")[0].innerHTML = "#" + this.id;
         clone.querySelector("article").id = "div_" + this.id;
         explorer.appendChild(clone);
     },
@@ -66,6 +113,18 @@ for (p = 1; p <= page_count; p++) {
     // [id range (ex. m1->m20), fetched?]
 }
 var counter = 1
+function tags(tagBlob) {
+    var tagsArr = []
+    if (tagBlob != "") {
+        for (l = 0; l < tagBlob.length; l++) {
+            var tagID = tagBlob[l];
+            tagsArr.push(tagsAll.get(tagID));
+        }
+        return tagsArr;
+    } else {
+        return [];
+    }
+}
 async function parseCSV() {
     Papa.parse("media.csv", {
         header: true,
@@ -76,38 +135,18 @@ async function parseCSV() {
             if (data["filetype"] !== undefined) {
                 mediaNew["id"] = data["id"];
                 mediaNew["filetype"] = data["filetype"];
+                mediaNew["tags"] = data["tags"];
                 mediaCSVData[counter] = mediaNew;
                 counter += 1;
             }
         },
         complete: function() {
-            init_media()
+            init_media();
         }
     })
     return true;
 }
 
-
-let tagsAll = new Map();
-tagsAll.set("Q", {name: "Qu3stion",    class: "qu3stion",    color: "#FFF", description: ""});
-tagsAll.set("X", {name: "3xclamation", class: "3xclamation", color: "#FFF", description: ""});
-tagsAll.set("C", {name: "C0mma",       class: "c0mma",       color: "#FFF", description: ""});
-tagsAll.set("A", {name: "Amp3rsand",   class: "amp3rsand",   color: "#FFF", description: ""});
-tagsAll.set("E", {name: "Creator",     class: "creator",     color: "#FFF", description: ""});
-tagsAll.set("G", {name: "Green",       class: "green",       color: "#FFF", description: ""});
-tagsAll.set("P", {name: "...Peri0d??", class: "peri0d",      color: "#FFF", description: ""});
-//
-tagsAll.set("l", {name: "Lore",         class: "lore",       color: "#FFF", description: ""});
-tagsAll.set("i", {name: "Intro",        class: "intro",      color: "#FFF", description: "First time a character appears!"});
-tagsAll.set("s", {name: "Silly",        class: "silly",      color: "#FFF", description: ""});
-tagsAll.set("z", {name: "Spooky",       class: "spooky",     color: "#FFF", description: ""});
-tagsAll.set("m", {name: "Mean",         class: "mean",       color: "#FFF", description: "WHEN THEY'RE MEAN TO QU3STION    ON TWITTER:"});
-tagsAll.set("n", {name: "Nice",         class: "nice",       color: "#FFF", description: "WHEN THEY'RE [Nice! :3] TO QU3STION    ON TWITTER:"});
-tagsAll.set("e", {name: "Explosion",    class: "explosion", color: "#FFF", description: ""});
-tagsAll.set("f", {name: "Fish",         class: "fish",       color: "#FFF", description: ""});
-tagsAll.set("a", {name: "Animal",       class: "animal",     color: "#FFF", description: ""});
-tagsAll.set("g", {name: "Games",        class: "Gaming",     color: "#FFF", description: ""});
-tagsAll.set("d", {name: "Drawings",     class: "drawings",   color: "#FFF", description: ""});
 
 async function load() {
     var check = await parseCSV();
@@ -122,6 +161,7 @@ async function init_media() {
         let data = mediaCSVData[x];
         template["id"] = data["id"];
         template["filetype"] = data["filetype"];
+        template["tags"] = tags(data["tags"]);
         mediaProcessedData[data["id"]] = template;
     }
     return true;
